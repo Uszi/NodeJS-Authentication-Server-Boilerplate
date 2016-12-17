3639b16a4dd0837a13830cf5c8ceab0aac0d14b7
Mateusz Grzywa <shatens@gmail.com>
2016-12-17 20:06:57 +0100

    added cars support

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:auth/auth');
+
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+*/
+
+app.use(cors);
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
